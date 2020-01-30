2226df37e024617fa99ad6c0aecdc458e211ea62
Hugo Simon <simon.hugo.p@gmail.com>
2020-01-30 15:12:13 +0100

    front login et register

--- app/src/main/java/com/example/hakaticket/ScanActivity.java
+++ app/src/main/java/com/example/hakaticket/ScanActivity.java
@@ -1,14 +1,103 @@
 package com.example.hakaticket;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
-public class ScanActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.zxing.Result;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class ScanActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    ZXingScannerView ScannerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
+        ScannerView = (ZXingScannerView)this.findViewById(R.id.scanview);
+        Dexter.withActivity(this).withPermission(Manifest.permission.CAMERA).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse response) {
+                ScannerView.setResultHandler(ScanActivity.this);
+                ScannerView.startCamera();
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse response) {
+                Toast.makeText(ScanActivity.this,"vous devez accepter cette permission", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+
+            }
+        }).check();
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomnav);
+        bottomNavigationView.setSelectedItemId(R.id.nav_qrcode);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.nav_account:
+                        startActivity(new Intent(getApplicationContext(), ScanActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.nav_qrcode:
+                        startActivity(new Intent(getApplicationContext(), ScanActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.nav_ticket:
+                        startActivity(new Intent(getApplicationContext(), ScanActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+
+    @Override
+    public void handleResult(Result result) {
+        Toast toast = Toast.makeText(getApplicationContext(), result.getText(), Toast.LENGTH_SHORT);
+        toast.show();
+        onBackPressed();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        ScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+        ScannerView.startCamera();          // Start camera on resume
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        ScannerView.stopCamera();           // Stop camera on pause
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ScannerView.stopCamera();
     }
 }
